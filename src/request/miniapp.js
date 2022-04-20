4d09ec8361982237074bba4e911b3dd1c5944e01
Logan <2448745034@qq.com>
2022-04-20 14:56:42 +0800

    网络请求与校验器模块

--- src/request/miniapp.js
+++ src/request/miniapp.js
@@ -1,135 +1,99 @@
-import store from '@/store'
-import { appletLogin } from '@/api/user'
-const baseUrl = process.env.VUE_APP_BASE_API
-const debounce = (fn, delay = 300) => {
-    let timer = null
-    return function (...args) {
-        clearTimeout(timer)
-        timer = setTimeout(() => {
-            fn.apply(this, args)
-        }, delay);
-    }
-}
-const toLogin = debounce((openid) => {
-    uni.navigateTo({
-        url: `/pages/login/index?openid=${openid}`
-    });
-})
-const uniToast = (options) => {
-    Promise.resolve().then(() => {
-        uni.showToast(options)
-    })
-}
-class Http {
+import Interceptor from './interceptor'
+
+// bases-uytrew 
+const BASE_URL = process.env.VUE_APP_BASE_URL,
+      PREFIX = '',
+      SUFFIX = '',
+      GLOBAL = wx;
+
+
+// main
+class Request extends Interceptor {
+
     constructor() {
-        this.subscribers = []
-        this.isRefrehToken = false
+        super()
+        this.__initConfig()
+        this.__initInterceptors()
+        this.isAbsolute = /^[a-z]+:\/\/\w+\.\w+/i
+    }
+
+    // 初始化拦截器
+    __initInterceptors() {
+        this.requetInterceptors = []
+        this.responseInterceptors = []
+    }
+
+    // 初始化配置
+    __initConfig() {
+        this.$config = {
+            prefix: PREFIX,
+            suffix: SUFFIX,
+            baseUrl: BASE_URL,
+            confirm: false,
+            toast: false,
+            loading: false
+        }
+    }
+
+    // 创建请求
+    request(options) {
+        let requetInterceptors = this.requetInterceptors,
+            responseInterceptors = this.responseInterceptors,
+            config = this.mergeConfig(options),
+            promise = Promise.resolve(config);
+
+        const forEach = (interceptors) => { 
+            let len = interceptors.length, index = 0
+            while (index < len) {
+                let interceptor = interceptors[index++]
+                if (interceptor) promise = promise.then(interceptor);
+            }
+        }
+        
+        forEach(requetInterceptors)
+        promise = this.send(config)
+        forEach(responseInterceptors)
+
+        return promise
+    }
+
+    // 发送请求
+    send(config) {
+        console.log("config", config)
+        return new Promise((resolve, reject) => {
+            config.success = resolve
+            config.fail = reject
+            GLOBAL.request(config)
+        })
+    }
+
+    // 合并配置
+    mergeConfig(options) {
+        let url = options.url, config = Object.assign(this.$config, options)
+        if (!this.isAbsolute.test(url)) {
+            config.url = config.baseUrl + config.prefix + url + config.suffix 
+        }
+        return config
+    }
+    
+    getToken() {
 
     }
-    // 添加订阅
-    _addSubScribe(callback) {
-        this.subscribers.push(callback)
-    }
-    // 执行所有并清空
-    _onAccessTokenFetched() {
-        const subscribers = this.subscribers.splice(0)
-        subscribers.forEach((callback) => {
-            callback()
-        })
-    }
-    // 重新获取token
-    _refreshToken() {
-        this.isRefrehToken = true
-        uni.login({
-            success: async ({ code }) => {
-                const res = await appletLogin({ code })
-                if (res.code === 5) {
-                    // 需要绑定教务账户
-                    toLogin(res.data.applet_openid)
-                }
-                if (res.code === 0) {
-                    // 更新token
-                    store.dispatch("setUserInfo", {
-                        ...res.data.info,
-                        token: res.data.token,
-                    });
-                    // 执行队列里的请求
-                    this._onAccessTokenFetched()
-                }
-                this.isRefrehToken = false
-            },
-            fail: () => {
-                uniToast({
-                    icon: "error",
-                    title: "获取code失败",
-                });
-            },
-        });
-    }
-    // 当token失效时 添加当前请求到队列里
-    _onTokenExpired(options, resolve, reject) {
-        this._addSubScribe(() => {
-            this.request(options).then(resolve).catch(reject)
-        })
-        !this.isRefrehToken && this._refreshToken()
-    }
-    request(options) {
-        const { url, data, loading, header = {}, auth = true, showToast = false } = options
-        return new Promise((resolve, reject) => {
-            // 需要鉴权的接口必须有token
-            if (auth) {
-                if (!store.getters.token) {
-                    this._onTokenExpired(options, resolve, reject)
-                    return
-                }
-                header.token = store.getters.token
-            }
-            loading && uni.showLoading({
-                title: "加载中",
-            });
-            // console.log(`${url} >>参数：`, data)
-            const _url = (baseUrl + url).replace('\'', '')
-            // console.log("_url", _url);
-            uni.request({
-                ...options,
-                url: _url,
-                data,
-                header,
-                success: (response) => {
-                    const { data } = response
-                    if (data.code === 3001) {
-                        this._onTokenExpired(options, resolve, reject)
-                        return
-                    }
-                    if (data.code !== 0 && data.code !== 5) {
-                        uniToast({
-                            icon: 'none',
-                            title: data.message || '请求失败'
-                        })
-                        // console.log(`${url} >>error：`, data)
-                        return reject(data)
-                    }
-                    if (showToast) {
-                        uniToast({
-                            icon: 'none',
-                            title: data.message
-                        })
-                    }
-                    resolve(data)
-                },
-                fail: (error) => {
-                    // console.log(`${url} >>error：`, error)
-                    uniToast({
-                        icon: 'error',
-                        title: error.errMsg || '稍后再试'
-                    })
-                    reject(error)
-                },
-                complete: () => {
-                    loading && uni.hideLoading()
-                }
-            })
-        })
+
+    setToken() {
+
     }
 }
-export default new Http()
+
+const request = new Request()
+
+request.useRequestInterceptor(() => {
+
+})
+
+request.useRequestInterceptor(() => {
+    console.log("useRequestInterceptor", this, request)
+})
+
+
+export default request.request.bind(request)
--- src/request/utils.js
+++ src/request/utils.js
@@ -1,18 +1,133 @@
-/**
- * 合并路径
- * @param {string} baseURL 基础路径
- * @param {string} relativeURL 相对路径
- * @returns {string} 合并后的路径
- */
- export const combineURLs = (baseURL, relativeURL) => {
-	return relativeURL ? baseURL.replace(/\/+$/, '') + '/' + relativeURL.replace(/^\/+/, '') : baseURL
+
+export const isUndef = (v) => {
+	return v === undefined || v === null
+}
+
+export const isDef = (v) => {
+	return v !== undefined && v !== null
+}
+
+export const isObject = (v) => {
+	return v !== null && typeof v === 'object'
+}
+
+// 是否为原始值
+export const isPrimitive = (v) => {
+	return (v !== undefined && v !== nnull) && typeof v === 'string' || typeof v === 'number' || typeof v === 'symbol' || typeof v === 'boolean'
+}
+
+export const isNumber = (v) => {
+	return isNaN(parseFloat(v)) ? false : true
+}
+
+export const cachePureFunction = (fn) => {
+	var cache = Object.create(null)
+	return function cacheFn(args) {
+			return  cache[args] || (cache[args] = fn(args) )
+	}
+}
+
+
+export const toNumbe = (val) => {
+	return isNaN(n = parseFloat(val)) ? val : n
+}
+
+export const extend = (son, father) => {
+	for (let k in father) { son[k] = father[k] }
+	return son
+}
+
+export const ArrayToObject = (arr) => {
+ let obj = Object.create(null)
+ for (let i = 0, ii = arr.length; i < ii; i++) {
+		 let it = arr[i]
+			for (let k in it) {
+					obj[k] = it[k]
+			}
+ }
+ return obj
+}
+
+export const ObjectToArray = (obj) => {
+	let arr = []; Object.keys(obj).forEach((k, i) => arr[i] = { k: obj[k] } )
+	return arr
+}
+
+export const is = (x, y) => {
+	// +0 -0 NaN
+	return x === y ? x !== 0 || 1 / x === 1 / y : x !== x && y !== y
 }
 
 /**
- * 判断是否绝对路径
- * @param {string} url 路径
- * @returns {boolean} 返回真值表示绝对路径，假值表示非绝对路径
- */
-export const isAbsoluteURL = url => {
-	return /^([a-z][a-z\d\+\-\.]*:)?\/\//i.test(url)
+* Check if two values are loosely equal - that is,
+* if they are plain objects, do they have the same shape?
+* 检查两个值是否大致相等-即如果它们是普通对象，它们的形状是否相同？
+*/
+export const looseEqual = (a, b) => {
+	if (a === b) { return true }
+	var isObjectA = isObject(a);
+	var isObjectB = isObject(b);
+	if (isObjectA && isObjectB) {
+			try {
+			var isArrayA = Array.isArray(a);
+			var isArrayB = Array.isArray(b);
+			if (isArrayA && isArrayB) {
+					return a.length === b.length && a.every(function (e, i) {
+					return looseEqual(e, b[i])
+					})
+			} else if (a instanceof Date && b instanceof Date) {
+					return a.getTime() === b.getTime()
+			} else if (!isArrayA && !isArrayB) {
+					var keysA = Object.keys(a);
+					var keysB = Object.keys(b);
+					return keysA.length === keysB.length && keysA.every(function (key) {
+					return looseEqual(a[key], b[key])
+					})
+			} else {
+					/* istanbul ignore next */
+					return false
+			}
+			} catch (e) {
+			/* istanbul ignore next */
+			return false
+			}
+	} else if (!isObjectA && !isObjectB) {
+			return String(a) === String(b)
+	} else {
+			return false
+	}
+}
+	
+export const looseIndexOf = (arr, val) => {
+	for (var i = 0; i < arr.length; i++) {
+		if (looseEqual(arr[i], val)) { return i }
+	}
+	return -1
+}
+
+	
+export const once = (fn) => {
+	var called = false;
+	return function () {
+		if (!called) {
+			called = true;
+			fn.apply(this, arguments);
+		}
+	}
+} 
+
+export const copyDeepth = (val) => {
+	let copy = undefined ;
+	if (Array.isArray(val)) {
+			for (let i = 0, ii = val.length; i < ii; i++) { copy[i] = val[i] }
+	} else if (val && typeof val === 'object') {
+			for (let k in val) { copy[k] = val[k] }
+	} else {
+			copy = val
+	}
+	return copy
+}
+
+export const isAbsoluteURL = (url) =>{
+	return /^[a-z]+:\/\/\w+\.\w+/i.test(url)
 }
