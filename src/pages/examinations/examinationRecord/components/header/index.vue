a01f7610dff79473343923551b16aa30e4e34cf2
Logan <2448745034@qq.com>
2022-06-01 17:11:15 +0800

    考试记录

---
+++ src/pages/examinations/examinationRecord/components/header/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <view class="header">
+    <block v-if="model === '1' || model === '3'">
+      <view class="status status--success">正确</view>
+      <view class="status status--error">错误</view>
+      <view class="status status--none">未回答</view>
+    </block>
+    <block v-if="model === '2'">
+      <view class="status status--primary">已回答</view>
+      <view class="status status--none">未回答</view>
+    </block>
+  </view>
+</template>
+<script>
+export default {
+  name: "IHeader",
+  props: {
+    model: {
+      type: String,
+      default: "primary",
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  padding: 20rpx;
+  border-bottom: $uni-border;
+  .status {
+    width: calc(100% / 3);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &::before {
+      content: "";
+      width: 32rpx;
+      height: 32rpx;
+      border-radius: 50%;
+      margin-right: 16rpx;
+    }
+    &--primary::before {
+      background-color: $uni-color-primary;
+    }
+    &--success::before {
+      background-color: $uni-answer-success;
+    }
+    &--error::before {
+      background-color: $uni-answer-error;
+    }
+    &--none::before {
+      background-color: #ddd;
+    }
+  }
+}
+</style>
---
+++ src/pages/examinations/examinationRecord/components/single/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="single">
+    <view class="quetion-content">
+      <u-parse :content="options.title" />
+    </view>
+    <Select :options="options.option" :value="checkedAnswer" :correct-answer="correctAnswer" @change="onChangeOpt"  />
+    <AnswerAnalysis v-if="correctAnswer" :question="options" :userAnswer="checkedAnswer" />
+  </div>
+</template>
+<script>
+import uParse from "@/components/gaoyia-parse/parse.vue";
+import AnswerAnalysis from "../answerAnalysis/index.vue";
+import Select from "../select/index";
+
+export default {
+  name: "single",
+  components: {
+    AnswerAnalysis,
+    uParse,
+    Select,
+  },
+
+  props: {
+    options: {
+      type: Object,
+      default: () => ({
+        option: [],
+        title: "",
+      }),
+    },
+    userAnswer: {
+      type: [Array, String, Number],
+      default: "",
+    },
+  },
+  data() {
+    return {
+      correctAnswer: '',
+      checkedAnswer: '',
+    };
+  },
+  created() {
+    if (this.options.user_answer.length) {
+      this.correctAnswer = this.options.true_answer.map(item => +item)
+      this.checkedAnswer = this.options.user_answer.map(item => +item)[0]
+      // console.log("single", this.options.user_answer, this.checkedAnswer)
+    }
+  },
+  methods: {
+    onChangeOpt(val) {
+        // console.log('single onChangeOpt', val);
+        this.checkedAnswer = val
+        this.correctAnswer = this.options.true_answer.map(item => +item)
+        let data = { id: this.options.id, answer: [val] }
+        this.$emit("change", data);
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.quetion-content {
+  margin-bottom: 20rpx;
+}
+</style>
