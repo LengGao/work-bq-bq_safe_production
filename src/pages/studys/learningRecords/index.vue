7cf6cfb96435d3073636dacd7c25b2c02c44aed9
Logan <2448745034@qq.com>
2022-06-17 11:18:19 +0800

    学习记录表

--- src/pages/studys/learningRecords/index.vue
+++ src/pages/studys/learningRecords/index.vue
@@ -3,118 +3,104 @@
     <custom-header :title="defaultTitle"></custom-header>
 
     <view class="main">
+      <view class="title">学习记录表</view>
+
       <view class="table">
         <view class="row">
-          <view class="col">学习者姓名</view>
-          <view class="col">{{ tableData.name }}</view>
+          <view class="col type-1">学习者姓名</view>
+          <view class="col type-2">{{ tableData.name }}</view>
         </view>
         <view class="row">
-          <view class="col">身份证号码</view>
-          <view class="col">{{ tableData.id_card }}</view>
+          <view class="col type-1">身份证号码</view>
+          <view class="col type-2">{{ tableData.id_card }}</view>
         </view>
         <view class="row">
-          <view class="col">学习者编号</view>
-          <view class="col">{{ tableData.stunum }}</view>
+          <view class="col type-1">学习者编号</view>
+          <view class="col type-2">{{ tableData.stunum }}</view>
         </view>
         <view class="row">
-          <view class="col">申请培训类别</view>
-          <view class="col">{{ tableData.busitype }}</view>
+          <view class="col type-1">申请培训类别</view>
+          <view class="col type-2">{{ tableData.busitype }}</view>
         </view>
         <view class="row">
-          <view class="col">申请培训类型</view>
-          <view class="col">{{ tableData.apcategory }}</view>
+          <view class="col type-1">申请培训类型</view>
+          <view class="col type-2">{{ tableData.apcategory }}</view>
         </view>
-        <view class="row">
-          <view class="col">规定培训学时</view>
-          <view class="col">{{ tableData.total_hour }}</view>
 
-          <view class="col">累计完成学时</view>
-          <view class="col">{{ tableData.completed_hour }}</view>
+        <view class="row">
+          <view class="col type-3">规定培训学时</view>
+          <view class="col type-4">{{ tableData.total_hour }}</view>
+
+          <view class="col type-3">累计完成学时</view>
+          <view class="col type-4">{{ tableData.completed_hour }}</view>
         </view>
         <view class="row">
-          <view class="col">学习开始时间</view>
-          <view class="col">{{ tableData.start_date }}</view>
+          <view class="col type-3">学习开始时间</view>
+          <view class="col type-4">{{ tableData.start_date }}</view>
 
-          <view class="col">学习结束时间</view>
-          <view class="col">{{ tableData.end_date }}</view>
+          <view class="col type-3">学习结束时间</view>
+          <view class="col type-4">{{ tableData.end_date }}</view>
         </view>
+
         <view v-for="(item, index) in listData.data" :key="index">
           <view class="row">
-            <view class="col">课程名称</view>
-            <view class="col">{{ item.course_title }}</view>
+            <view class="col type-3">课程名称</view>
+            <view class="col type-4"><text style="margin: 0px 10rpx;">{{ item.course_title }}</text></view>
 
-            <view class="col">课程编号</view>
-            <view class="col">{{ item.course_id }}</view>
+            <view class="col type-3">课程编号</view>
+            <view class="col type-4">{{ item.course_id }}</view>
           </view>
           <view class="row">
-            <view class="col">单次学习开始时间</view>
-            <view class="col">单次学习结束时间</view>
-            <view class="col">IP地址</view>
+            <view class="col type-5">单次学习开始时间</view>
+            <view class="col type-5">单次学习结束时间</view>
+            <view class="col type-5">IP地址</view>
           </view>
           <view class="row">
-            <view class="col">{{ item.starttime }}</view>
-            <view class="col">{{ item.endtime }}</view>
-            <view class="col">{{ item.ip }}</view>
+            <view class="col type-6">{{ item.starttime }}</view>
+            <view class="col type-6">{{ item.endtime }}</view>
+            <view class="col type-6">{{ item.ip }}</view>
           </view>
         </view>
+      </view>
+
+      <view class="seal" v-if="tableData.certificate_stamp">
+        <image class="seal-img" :src="tableData.certificate_stamp" mode="aspectFit" />
       </view>
     </view>
   </view>
 </template>
 
 <script>
+import CustomHeader from "@/components/custom-header"
 import { buildLearnRecord } from '@/api/course'
 
 export default {
+  components: {
+    CustomHeader
+  },
   data() {
     return {
       defaultTitle: '学习记录',
       course_id: '',
-      tableData: {
-        "name": "XXX",
-        "id_card": "1***********5",
-        "stunum": "100010000000055",
-        "busitype": "取证",
-        "apcategory": "特种作业人员",
-        "total_hour": 3,
-        "start_date": "2022-06-07",
-        "end_date": "2022-06-14",
-        "completed_hour": 3,
-        "org_name": "xxx机构",
-        "today": "2022年06月06日",
-        "certificate_stamp": "http://xxx.png",
-      },
-
-      listData: {
-        "total": 1,
-        "per_page": 20,
-        "current_page": 1,
-        "last_page": 1,
-        "data": [
-          {
-            "course_id": 26,
-            "course_title": "低压电工作业初训",
-            "starttime": "2022-06-14 11:25:04",
-            "endtime": "2022-06-14 11:32:15",
-            "ip": "127.0.0.1"
-          }
-        ]
-      }
+      tableData: {},
+      listData: {}
     }
   },
   onLoad(options) {
     let { course_id } = options
     this.course_id = course_id
-    // this.buildLearnRecord()
+    this.buildLearnRecord()
   },
   methods: {
     async buildLearnRecord() {
+      uni.showLoading({ title: '记录生成中' })
       let params = { course_id: this.course_id }
       let res = await buildLearnRecord(params)
       if (res.code === 0) {
         this.tableData = res.data
-        this.listData = res.data.list
+        this.listData = res.data.record
       }
+      uni.hideLoading()
     }
   }
 }
@@ -123,5 +109,92 @@ export default {
 <style lang="scss" scoped>
 .learning-records {
   width: 100%;
+  padding-bottom: var(--window-bottom);
 }
+
+.main {
+  position: relative;
+  padding: 30rpx 20rpx;
+}
+
+.title {
+  width: 100%;
+  font-size: 32rpx;
+  color: #333;
+  text-align: center;
+  margin-bottom: 30rpx;
+}
+
+.table {
+  display: flex;
+  flex-flow: column nowrap;
+  border-bottom: 1px solid #666;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+  min-height: 60rpx;
+  font-size: 24rpx;
+  line-height: 60rpx;
+  border-collapse: collapse;
+  border: 1px solid #666;
+  border-bottom: none;
+}
+
+.col {
+  flex: 1;
+  text-align: center;
+  font-size: 24rpx;
+  border-right: 1px solid #666;;
+}
+
+.col:last-child {
+  border-right: none;
+}
+
+.type-1 {
+  flex: 1;
+}
+
+.type-2 {
+  flex: 3;
+}
+
+.type-3 {
+  flex: 1;
+}
+
+.type-4 {
+  flex: 1;
+}
+
+.type-5 {
+  flex: 1.5;
+}
+
+.type-5:nth-of-type(3) {
+  flex-grow: 1;
+}
+
+.type-6 {
+  flex: 1.5;
+}
+
+.type-6:nth-of-type(3) {
+  flex-grow: 1;
+}
+
+.seal {
+  position: absolute;
+  right: 20rpx;
+  bottom: -120rpx;
+  .seal-img {
+    
+    width: 160rpx;
+    height: 160rpx;
+  }
+}
+
+
 </style>
